91698d55616c8bb7d2b787af8bd1a1d503e5186b
bsao17 <declic62@gmail.com>
2021-07-12 12:24:25 +0200

    Services page update

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ function App(props) {
             <Nav className="mr-auto">
               <Nav.Link onClick={()=>{setComponent(<Home/>)}}><i className="fas fa-home"/> Accueil</Nav.Link>
               <Nav.Link onClick={()=>{setComponent(<About/>)}}><i className="far fa-address-card"/> A propos</Nav.Link>
-              <Nav.Link onClick={()=>{setComponent(<Services/>)}}><i className="fas fa-briefcase"/> Mes Services</Nav.Link>
               <Nav.Link href="" ><i className="fab fa-wordpress"></i> Blog</Nav.Link>
-              <Nav.Link onClick={()=>{setComponent(<Portfolio/>)}}><i className="fas fa-briefcase"/>Portfolio</Nav.Link>
+              <Nav.Link onClick={()=>{setComponent(<Portfolio/>)}}><i className="fas fa-briefcase"/> Portfolio</Nav.Link>
+              <Nav.Link onClick={()=>{setComponent(<Contact/>)}}><i className="fas fa-address-book"/> Contactez-moi</Nav.Link>
 
-              <NavDropdown title="Plus" id="basic-nav-dropdown">
+              {/* <NavDropdown title="Plus" id="basic-nav-dropdown">
                 <NavDropdown.Item onClick={()=>setComponent(<Contact/>)}><i className="fas fa-address-book"/> Contactez-moi</NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
