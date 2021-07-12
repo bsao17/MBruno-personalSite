40f2c1759bed90699a138ede281e83a084e0df1c
bsao17 <declic62@gmail.com>
2021-07-12 19:02:17 +0200

    Add carousel pictures and titles

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -16,41 +16,34 @@ export default class Services extends Component {
                 <Carousel.Item interval={5000}>
                   <img
                     className="d-block w-100"
-                    src="https://loremflickr.com/320/240"
-                    alt="First slide"
+                    src="images/carousel/app-web.jpg"
+                    alt=""
                   />
                   <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>
-                      Nulla vitae elit libero, a pharetra augue mollis interdum.
-                    </p>
+                    <h1 className={serviceStyle.carouselImage}>App &amp; Site Web</h1>
+                    
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={5000}>
                   <img
                     className="d-block w-100"
-                    src="https://loremflickr.com/320/240"
+                    src="images/carousel/app-mobile.jpg"
                     alt="Second slide"
                   />
                   <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </p>
+                    <h1 className={serviceStyle.carouselImage}>Second slide label</h1>
+                    
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
-                    src="https://loremflickr.com/320/240"
+                    src="images/carousel/copywriting.png"
                     alt="Third slide"
                   />
                   <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl
-                      consectetur.
-                    </p>
+                    <h1 className={serviceStyle.carouselImage}>Copywriting &amp; Marketing</h1>
+                    
                   </Carousel.Caption>
                 </Carousel.Item>
               </Carousel>
