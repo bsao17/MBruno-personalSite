5aed9634d3ea2537592e812475eae41c12278bf4
bsao17 <declic62@gmail.com>
2021-07-15 22:27:45 +0200

    Working on portofolio responsive

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,19 +3,21 @@ import styleFooter from './footer.module.scss'
 
 export const Footer = () => {
     return (
-        <div className={styleFooter.container}>
-            <div className={styleFooter.link}>
-                {/*TODO: add facebook twitter instagram links*/}
-                <a href="www.facebook.com" className={styleFooter.facebook}><i class="fab fa-facebook-square"/></a>
-                <a href="www.instagram.com" className={styleFooter.instagram}><i class="fab fa-instagram"></i></a>                
-                <a href="www.twitter.com" className={styleFooter.twitter}><i class="fab fa-twitter-square"></i></a>
-                <a href="www.github.com" className={styleFooter.github}><i class="fab fa-github-square"></i></a>
-                <a href="www.linkedin.com" className={styleFooter.linkedin}><i class="fab fa-linkedin"></i></a>
-            </div>
+        <div className={styleFooter.footer}>
+            <div className={styleFooter.container}>
+                <div className={styleFooter.link}>
+                    {/*TODO: add facebook twitter instagram links*/}
+                    <a href="https://www.facebook.com/devwebdino" className={styleFooter.facebook}><i className="fab fa-facebook-square"/></a>
+                    <a href="https://www.instagram.com" className={styleFooter.instagram}><i className="fab fa-instagram"/></a>
+                    <a href="https://www.twitter.com" className={styleFooter.twitter}><i className="fab fa-twitter-square"/></a>
+                    <a href="https://github.com/bsao17" className={styleFooter.github}><i className="fab fa-github-square"/></a>
+                    <a href="https://www.linkedin.com/in/bruno-mehddeb-1349111a3/" className={styleFooter.linkedin}><i className="fab fa-linkedin"></i></a>
+                </div>
 
-            <div className={styleFooter.copyright}>
-                <span>Copyright © 2021 Mehddeb Bruno </span>
-                <span>All rights reserved.</span>
+                <div className={styleFooter.copyright}>
+                    <span>Copyright © 2021 Mehddeb Bruno </span>
+                    <span>All rights reserved.</span>
+                </div>
             </div>
         </div>
     )
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -13,14 +13,13 @@ export default function Portfolio(props) {
         item.className = style.modale
         item.innerHTML = d
         globalContainer = document.getElementById('globalContainer')
-        const modale = globalContainer.appendChild(item)
-        return modale
+        return globalContainer.appendChild(item)
     }
 
-    const removeData = ()=>{
-        try{
+    const removeData = () => {
+        try {
             globalContainer.removeChild(item)
-        }catch {
+        } catch {
             console.error('erreur nodeChild')
         }
     }
@@ -30,37 +29,37 @@ export default function Portfolio(props) {
             <br/>
             <div className=" container-fluid d-flex flex-row flex-wrap justify-content-center align-items-center"
                  id={"globalContainer"}
-                 style={{height: "100vh"}}
                  data-aos="zoom-out"
                  data-aos-easing="ease-out-cubic"
                  data-aos-duration="2000"
             >
                 <BgAbout/>
-                <div onMouseEnter={()=> showData("Voici un pure projet d'integration web, cela signifie la reproduction à l'identique d'une maquette réalisée par un designer. Ceci est le travail de base de tout développeurs !")}
-                     onMouseLeave={removeData}
-                     id={'cardOne'}
-                     className=" card col-lg-3 col-md-6 col-12 m-1 rounded-3"
-                >
 
+                <div
+                    onMouseEnter={() => showData("Voici un pure projet d'integration web, cela signifie la reproduction à l'identique d'une maquette réalisée par un designer. Ceci est le travail de base de tout développeurs !")}
+                    onMouseLeave={removeData}
+                    id={'cardOne'}
+                    className="card col-lg-4 col-md-6 col-12 m-5 p-2 rounded-3">
                     <div
                         className="
                         d-flex 
                         flex-column  
                         justify-content-center
-                        align-items-center  
+                        align-items-center
                         mx-2
                         align-items-center">
                         WebAgency
                     </div>
                     <a href="https://bsao17.github.io/webAgency/">
                         <img src="images/sites/agency.jpg"
-                             className="w-100"
+                             className="w-100 rounded-bottom"
                              alt="agency site"/>
                     </a>
                 </div>
-                <div onMouseEnter={()=> showData("L'objectif de celui-ci, est la réalisation d'une SPA pour single page application. Le principe est assez simple en soit, le but étant comme son nom l'indique, de créer une appliction monopage ou tout le code est chargé à la première ouverture du site afin que tout se deroule sur celle ci sans jamais avoir à en changer. Comme une application native sur votre ordinateur mais à travers un navigateur web ou mobile!")}
-                     onMouseLeave={removeData}
-                    className="card col-lg-3 col-md-6 col-12 m-1 rounded-3">
+                <div
+                    onMouseEnter={() => showData("L'objectif de celui-ci, est la réalisation d'une SPA pour single page application. Le principe est assez simple en soit, le but étant comme son nom l'indique, de créer une appliction monopage ou tout le code est chargé à la première ouverture du site afin que tout se deroule sur celle ci sans jamais avoir à en changer. Comme une application native sur votre ordinateur mais à travers un navigateur web ou mobile!")}
+                    onMouseLeave={removeData}
+                    className="card col-lg-4 col-md-6 col-12 m-5 p-2 rounded">
                     <div
                         className="
                         d-flex 
@@ -70,11 +69,12 @@ export default function Portfolio(props) {
                         bikeRenting
                     </div>
                     <a href="https://bsao17.github.io/Project3_OC/" target="blank"><img
-                        src="images/sites/bikeRenting.jpg" className="w-100" alt="agency site"/></a>
+                        src="images/sites/bikeRenting.jpg" className="w-100 rounded-bottom" alt="agency site"/></a>
                 </div>
-                <div onMouseEnter={()=> showData("Ici, la création du site de l'office de tourisme de la ville d'Ireki en Alaska, ne la cherchez pas car elle n'existe pas, le but étant ici d'utiliser un CMS (content management system), donc un système de gestion de contenu pour accélérer et faciliter la création. Attenntion tout de même à l'apparente simplicité trompeuse de ce genre d'outils, entre autre wordpress dans le cas présent")}
-                     onMouseLeave={removeData}
-                    className="card col-lg-3 col-md-6 col-12 m-1 rounded-3">
+                <div
+                    onMouseEnter={() => showData("Ici, la création du site de l'office de tourisme de la ville d'Ireki en Alaska, ne la cherchez pas car elle n'existe pas, le but étant ici d'utiliser un CMS (content management system), donc un système de gestion de contenu pour accélérer et faciliter la création. Attenntion tout de même à l'apparente simplicité trompeuse de ce genre d'outils, entre autre wordpress dans le cas présent")}
+                    onMouseLeave={removeData}
+                    className="card col-lg-4 col-md-6 col-12 m-5 p-2 rounded-3">
                     <div
                         className="
                         d-flex 
@@ -84,15 +84,18 @@ export default function Portfolio(props) {
                         Ireki
                     </div>
                     <a href="http://devwebdino.fr/"
-                       target="blank"><img
-                        src="images/sites/ireki.jpg"
-                        className="w-100"
-                        alt="agency site"/>
+                       target="blank">
+                        <img
+                            src="images/sites/ireki.jpg"
+                            className="w-100 rounded-bottom"
+                            alt="agency site"
+                        />
                     </a>
                 </div>
-                <div onMouseEnter={()=> showData("Voici un projet en langage PHP pour le développement du site et mySQL pour la gestion de la base de données. Ce site est celui de l'écrivain Jean Forteroche, qui écrit et distribue son nouveau livre au fur et à mesure par l'intermédiaire de son blog")}
-                     onMouseLeave={removeData}
-                    className="card col-lg-3 col-md-6 col-12 m-1 rounded-3">
+                <div
+                    onMouseEnter={() => showData("Voici un projet en langage PHP pour le développement du site et mySQL pour la gestion de la base de données. Ce site est celui de l'écrivain Jean Forteroche, qui écrit et distribue son nouveau livre au fur et à mesure par l'intermédiaire de son blog")}
+                    onMouseLeave={removeData}
+                    className="card col-lg-4 col-md-6 col-12 m-5 p-2 rounded-3">
                     <div
                         className="
                         d-flex 
@@ -102,12 +105,13 @@ export default function Portfolio(props) {
                         Jean Forteroche
                     </div>
                     <a href="http://blog-forteroche.devwebdino.com/?action=home" target="blank"><img
-                        src="images/sites/forteroche.jpg" className="w-100" alt="agency site"/>
+                        src="images/sites/forteroche.jpg" className="w-100 rounded-bottom" alt="agency site"/>
                     </a>
                 </div>
-                <div onMouseEnter={()=> showData("Et celui ci, est mon projet personnel. Le titre en soit est assez claire ! Il vise à tordre le coup aux idées reçus, comme quoi les développeurs juniors (donc débutants) seraient incapables de créer ou de mener à bien un projet en autonomie, en exposant leurs travaux sur ce blog")}
-                     onMouseLeave={removeData}
-                    className="card col-lg-3 col-md-6 col-12 m-1 rounded-3">
+                <div
+                    onMouseEnter={() => showData("Et celui ci, est mon projet personnel. Le titre en soit est assez claire ! Il vise à tordre le coup aux idées reçus, comme quoi les développeurs juniors (donc débutants) seraient incapables de créer ou de mener à bien un projet en autonomie, en exposant leurs travaux sur ce blog")}
+                    onMouseLeave={removeData}
+                    className="card col-lg-4 col-md-6 col-12 m-5 p-2 rounded-3">
                     <div
                         className="
                         d-flex 
@@ -118,11 +122,13 @@ export default function Portfolio(props) {
                     </div>
                     <a href="https://jmpm-e70d2.web.app/" target="blank">
                         <img src="images/sites/jmpm.jpg"
-                             className="w-100" alt="agency site"/>
+                             className="w-100 rounded-bottom" alt="agency site"/>
                     </a>
                 </div>
             </div>
-            <Footer/>
+            <div className={'d-inline-block w-100'}>
+                <Footer/>
+            </div>
         </>
     )
 }
