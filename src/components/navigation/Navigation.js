451b510dce699f2d10ac376e7759531f651c8f5d
bsao17 <declic62@gmail.com>
2022-04-02 12:04:01 +0200

    Add greensocks animation to modale menu responsive

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,13 +2,23 @@ import React from "react";
 import styles from "./navigation.module.scss"
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import {About, Contact, Home, Portfolio, WhoIAm} from "../index";
+import {gsap} from "gsap"
 
 export default function Navigation() {
     const [responsiveMenu, setResponsiveMenu] = React.useState(true)
+    const modaleMenu = React.useRef(null)
     const mobileMenuShow = () => {
         setResponsiveMenu(!responsiveMenu)
     }
-    console.log(responsiveMenu)
+    React.useEffect(()=>{
+        if(responsiveMenu === false){
+            gsap.to(modaleMenu.current, {translateY: "100%"})
+        }
+        else if(responsiveMenu){
+            gsap.to(modaleMenu.current, {translateY: "-100%"})
+        }
+    }, [responsiveMenu])
+
     return (
         <Router>
             <h1 className={responsiveMenu ? styles.closed : styles.closedShow} onClick={mobileMenuShow}>✕</h1>
@@ -18,7 +28,7 @@ export default function Navigation() {
                 <div className={styles.trait}/>
             </div>
             <div className={styles.container}>
-                <ul className={responsiveMenu ? styles.navLink : styles.navLinkShow}>
+                <ul ref={modaleMenu} className={responsiveMenu ? styles.navLink : styles.navLinkShow}>
                     <li><Link to={"/"}>Accueil</Link></li>
                     <li><Link to={"/about"}>A propos</Link></li>
                     <li><Link to={"/whoiam"}>Qui suis-je</Link></li>
